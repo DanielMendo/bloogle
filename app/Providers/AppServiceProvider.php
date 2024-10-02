c3938d2f60a0b0d59c642cc89c6458c37f9d6b38
Jesús Daniel Hernández Mendoza <danielmendohernan@hotmail.com>
2024-10-01 20:45:10 -0600

    Blogool versión 1.123 intento de arreglo 1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
+
     }
 }
